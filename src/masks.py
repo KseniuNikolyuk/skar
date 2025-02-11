b9caab627329f915ef59c25d411228cf24616c44
Nikolyuk.K <nikolukksenia0704@gmail.com>
2025-02-11 14:37:20 +0300

    обновление

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """функция для скрытия номера"""
-    card_number = str(card_number)
+    # card_number = str(card_number)
     block1 = card_number[:4]
     block2 = card_number[4:6]
     block3 = "**"
@@ -9,7 +9,8 @@ def get_mask_card_number(card_number: int) -> str:
     mask_card = f"{block1} {block2}{block3} {block4} {block5}"
     return mask_card
 
-def get_mask_account(account_number: int) -> str:
+
+def get_mask_account(account_number: str) -> str:
     """функция для скрытия номера"""
     block6 = account_number[-4:]
     mask_number = f"**{block6}"
