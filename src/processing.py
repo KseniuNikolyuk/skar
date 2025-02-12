d7520daf5e56492c893fd59ec9d15fbd76f041bf
Nikolyuk.K <nikolukksenia0704@gmail.com>
2025-02-12 13:57:22 +0300

    обновление+

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 
-def filter_by_state(lists: list[dict], state = "EXECUTED") -> list[dict]:
-    result =[]
-    for i in lists:
-        if i['state'] == 'EXECUTED':
+
+def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    функция сортировки списка словарей по статусу операции.
+    """
+    result = []
+    for i in operations:
+        if i["state"] == state:
             result.append(i)
     return result
 
-def sort_by_date(lists: list[dict], revers = True) -> list[dict]:
-    return sorted(lists, key=lambda date: datetime.strptime(date["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse= revers)
 
-
-
+def sort_by_date(operations: list[dict], revers: bool = True) -> list[dict]:
+    """
+    функция сортировки списка словатей по ключу даты.
+    """
+    return sorted(operations, key=lambda date: datetime.strptime(date["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=revers)
